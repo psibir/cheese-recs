94735c5f6e80bec320a5b28664da7efa21eedd89
Trevor Bloomfield <bloomfieldtm@gmail.com>
2023-05-11 19:57:32 -0500

    added app route to faq

--- app.py
+++ app.py
@@ -65,6 +65,10 @@ def search():
 def about():
     return render_template('about.html')
 
+@app.route('/faq')
+def about():
+    return render_template('FAQ.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
