58c21b6f347ccd175c8723057e118afa13e640e6
Trevor Bloomfield <bloomfieldtm@gmail.com>
2023-05-14 00:10:46 -0500

    added a need help link to a dropdown flavor menu

---
+++ cheese_quality_ranking.py
@@ -0,0 +1,23 @@
+#  this is a ranking  of cheese qualities that 
+#  influence the flavor of cheese
+
+
+#  cheese
+#  milk
+#  flavor
+#  aroma
+#  kind
+#  texture
+#  fat
+#  sodium
+#  rind
+#  origin
+#  family
+#  color
+#  description
+#  vegetarian
+
+#  TODO
+
+# write formulae for each cheese quality
+# weight each quality as a number that sum to 1 for influencing the flavor
