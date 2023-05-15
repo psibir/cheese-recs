680c10c3c4d1c8f5cf03f8674c5761056d7f32aa
Trevor Bloomfield <bloomfieldtm@gmail.com>
2023-05-15 01:28:01 -0500

    reverted changes, now trying read_csv method

--- cheese_recommender.py
+++ cheese_recommender.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from fuzzywuzzy import fuzz
 
 class CheeseRecommender:
     def __init__(self, cheese_file):
@@ -9,25 +8,24 @@ class CheeseRecommender:
         self.vectorizer = TfidfVectorizer()
 
     def get_recommendations(self, user_input=None, num_recommendations=5, start_index=0, exclude_words=None):
+        if not user_input:
+            return pd.DataFrame(columns=['cheese', 'milk', 'origin', 'region', 'kind', 'color', 'texture', 'flavor', 'aroma', 'description', 'producer'])
+
+        words = user_input.split()
+        if not any(word in self.df['cheese', 'milk', 'origin', 'region', 'kind', 'color', 'texture', 'flavor', 'aroma', 'description', 'producer'].tolist() for word in words):
+            return pd.DataFrame(columns=['cheese', 'milk', 'origin', 'region', 'kind', 'color', 'texture', 'flavor', 'aroma', 'description', 'producer'])
+
         self.df.fillna('', inplace=True)
         cheese_desc = self.df.apply(lambda x: ' '.join(x), axis=1)
         cheese_matrix = self.vectorizer.fit_transform(cheese_desc)
 
-        if user_input is None or not self._is_significant_match(user_input, self.df['cheese']):
-            return pd.DataFrame(columns=['cheese', 'milk', 'origin', 'region', 'kind', 'color', 'texture', 'flavor', 'aroma', 'description', 'producer'])
-
-        user_words = user_input.split()
-        total_scores = []
-        for word in user_words:
-            user_vector = self.vectorizer.transform([word])
-            sim_scores = cosine_similarity(user_vector, cheese_matrix).flatten()
-            total_scores.append(sim_scores)
-
-        averaged_scores = sum(total_scores) / len(user_words)
-        sim_indices = averaged_scores.argsort()[::-1][start_index:num_recommendations + start_index]
+        user_vector = self.vectorizer.transform([user_input])
+        sim_scores = cosine_similarity(user_vector, cheese_matrix).flatten()
+        sim_indices = sim_scores.argsort()[::-1][start_index:num_recommendations + start_index]
 
         recommendations = self.df.iloc[sim_indices][
-            ['cheese', 'milk', 'origin', 'region', 'kind', 'color', 'texture', 'flavor', 'aroma', 'description', 'producer']]
+            ['cheese', 'milk', 'origin', 'region', 'kind', 'color', 'texture', 'flavor', 'aroma', 'description',
+             'producer']]
         recommendations['origin'] = recommendations['origin'].str.split(',').str[0]
 
         if exclude_words:
@@ -47,15 +45,6 @@ class CheeseRecommender:
             return pd.DataFrame(columns=['cheese', 'milk', 'origin', 'region', 'kind', 'color', 'texture', 'flavor', 'aroma', 'description', 'producer'])
 
         return recommendations
-
-    def _is_significant_match(self, user_input, choices, threshold=70):
-        words = user_input.split()
-        for choice in choices:
-            match_scores = [fuzz.token_set_ratio(word, choice) for word in words]
-            avg_score = sum(match_scores) / len(words)
-            if avg_score >= threshold:
-                return True
-        return False
 
     @classmethod
     def get_all_cheeses(cls, cheese_file):
